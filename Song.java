1ab49865c9e5272f07bea9d16442ed7cabb94677
pwnd00d <austinpwnd00dzhou@gmail.com>
2015-05-27 09:56:58 -0500

    temp

---
+++ Song.java
@@ -0,0 +1,24 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.LineEvent.Type;
+
+public class Song implements Runnable
+{
+    private Clip clip;
+    private long delay;
+    
+    public static Song playNew(String f, long delay) throws Exception
+    {
+        Song s = new Song();
+        s.delay = delay;
+        s.clip = AudioSystem.getClip();
+        s.clip.open(AudioSystem.getAudioInputStream(new File(f)));
+        return s;
+    }
+    
+    public void run()
+    {
+        
+    }
+}
